d8e85dfe68fe1de46a371c4ad0c917bcb3dc40ff
Tanvir Alam <tanviralam@Tanvirs-MacBook-Pro.local>
2021-05-28 22:53:07 -0700

    Project skeleton

---
+++ src/main/java/com/tinyurl/urlfinder/UrlfinderApplication.java
@@ -0,0 +1,13 @@
+package com.tinyurl.urlfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinyurl/urlfinder/UrlfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyurl.urlfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
